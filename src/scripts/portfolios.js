961884359a3d38bfc87bb58c8e376b1857b5e3bd
Anisa Foroozan <aforooza@uwo.ca>
2026-04-08 14:53:14 -0400

    prev didn't work diff script

--- src/scripts/portfolios.js
+++ src/scripts/portfolios.js
@@ -1,5 +1,7 @@
 if (window.innerWidth <= 393) {
-  document.querySelectorAll('.dot').forEach(el => {
+  document.querySelectorAll(
+    '.ellipse ellipse, .ellipse1 ellipse, .ellipse2 ellipse, .ellipse3 ellipse, .ellipse4 ellipse, .ellipse5 ellipse'
+  ).forEach(el => {
     el.setAttribute('rx', 4);
     el.setAttribute('ry', 4);
   });

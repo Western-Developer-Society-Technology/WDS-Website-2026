16f3b3b632737cea3483eada0befdce129445a0e
Anisa Foroozan <aforooza@uwo.ca>
2026-04-07 21:42:00 -0400

    js for ellipse

--- src/scripts/portfolios.js
+++ src/scripts/portfolios.js
@@ -1,8 +1,6 @@
-const circles = document.querySelectorAll('.neon-circle');
-    circles.forEach(circle => {
-        circle.addEventListener('click', () => {
-            const isActive = circle.classList.contains('active');
-            circles.forEach(c => c.classList.remove('active')); // close others
-            if (!isActive) circle.classList.add('active'); // open clicked one
-        });
-    });
+if (window.innerWidth <= 393) {
+  document.querySelectorAll('.ellipse ellipse').forEach(el => {
+    el.setAttribute('rx', 4);
+    el.setAttribute('ry', 4);
+  });
+}

7c636be8533661aaa6a924b7d091f8a731ab2d5c
Anisa Foroozan <aforooza@uwo.ca>
2026-04-08 14:49:41 -0400

    js for ellipses

--- src/scripts/portfolios.js
+++ src/scripts/portfolios.js
@@ -1,5 +1,5 @@
 if (window.innerWidth <= 393) {
-  document.querySelectorAll('#ellipse, #ellipse1, #ellipse2, #ellipse3, #ellipse4, #ellipse5').forEach(el => {
+  document.querySelectorAll('.dot').forEach(el => {
     el.setAttribute('rx', 4);
     el.setAttribute('ry', 4);
   });
